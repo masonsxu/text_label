b451484e0b41fe80e41a498bb0f022b15ea61641
masonsxu <masonsxu@foxmail.com>
2021-03-15 20:40:39 +0800

    更新后端开放端口

--- vue.config.js
+++ vue.config.js
@@ -45,7 +45,7 @@ module.exports = {
     // 配置多个代理
     proxy: {
       "/api": {
-        target: "http://localhost:8080/api", // 要访问的接口域名
+        target: "http://localhost:5000/api", // 要访问的接口域名
         ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
