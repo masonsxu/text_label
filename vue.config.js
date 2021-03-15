d7a2838cda24ee5f7aad101074300febfb09ec54
masonsxu <masonsxu@foxmail.com>
2021-03-15 13:34:40 +0800

    更新vue构建配置

--- vue.config.js
+++ vue.config.js
@@ -7,14 +7,14 @@ module.exports = {
   //例如 https://www.my-app.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 baseUrl 为 /my-app/。
   //baseUrl 从 Vue CLI 3.3 起已弃用，请使用publicPath
   //baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  // publicPath: "./",
 
   // outputDir: 在npm run build 或 yarn build 时 ，生成文件的目录名称
-  outputDir: "mycli3",
+  outputDir: "dist",
   //用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
-  assetsDir: "assets",
+  assetsDir: "./static",
   //指定生成的 index.html 的输出路径  (打包之后，改变系统默认的index.html的文件名)
-  // indexPath: "myIndex.html",
+  indexPath: "./index.html",
   //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变)
   filenameHashing: false,
 
