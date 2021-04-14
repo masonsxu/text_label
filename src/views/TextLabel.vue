c06a77bf0b5a321bebd9fe0fdbc9b25b07bb0011
masonsxu <masonsxu@foxmail.com>
2021-04-14 17:30:37 +0800

    增加标注三元组选项的跟随效果；精简之前的选项跟随鼠标效果

--- src/views/TextLabel.vue
+++ src/views/TextLabel.vue
@@ -45,7 +45,7 @@
                 <el-button
                   id="verb_list"
                   size="small"
-                  @mousedown="verb_choose"
+                  @mousedown="verb_choose(item.text, $event)"
                   >{{ item.label }}</el-button
                 ></el-tooltip
               >
@@ -67,10 +67,10 @@
         </el-col>
         <el-col :span="12">
           <el-table :data="spo_list" height="160" border style="width: 100%">
-            <el-table-column prop="subject_type" label="主语">
+            <el-table-column prop="subject_type" label="主语(S)">
             </el-table-column>
-            <el-table-column prop="predicate" label="谓语"> </el-table-column
-            ><el-table-column prop="object_type" label="宾语">
+            <el-table-column prop="predicate" label="谓语(P)"> </el-table-column
+            ><el-table-column prop="object_type" label="宾语(O)">
             </el-table-column> </el-table
         ></el-col>
       </el-row>
@@ -81,13 +81,27 @@
       v-show="flag == null ? show : !show"
     >
       <el-radio-button
-        v-for="item in options"
+        v-for="item in lable_options"
         :key="item.value"
         :label="item.label"
         :value="item.value"
         @click.prevent="textLabel(item.value)"
       ></el-radio-button>
     </el-radio-group>
+    <el-select
+      v-model="value"
+      placeholder="请选择"
+      v-show="predicate == null ? show : !show"
+    >
+      <el-option
+        v-for="item in predicate_options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+        @click.prevent="preLabel(item.value)"
+      >
+      </el-option>
+    </el-select>
   </div>
 </template>
 
@@ -105,13 +119,15 @@ export default {
       flag: null,
       text: null,
       value: null,
+      predicate: null,
       abstract: null,
       text_pre: null,
       text_label: null,
+      spo_dict: {},
       spo_list: [],
       table_data: [],
       abstract_label: [],
-      options: [
+      lable_options: [
         {
           value: "LOC",
           label: "LOC"
@@ -136,6 +152,56 @@ export default {
           value: "O",
           label: "O"
         }
+      ],
+      predicate_options: [
+        {
+          value: "祖籍",
+          label: "祖籍"
+        },
+        {
+          value: "父亲",
+          label: "父亲"
+        },
+        {
+          value: "总部地点",
+          label: "总部地点"
+        },
+        {
+          value: "出生地",
+          label: "出生地"
+        },
+        {
+          value: "面积",
+          label: "面积"
+        },
+        {
+          value: "简称",
+          label: "简称"
+        },
+        {
+          value: "作者",
+          label: "作者"
+        },
+        {
+          value: "成立日期",
+          label: "成立日期"
+        },
+        {
+          value: "总部地点",
+          label: "总部地点"
+        },
+        {
+          value: "出生日期",
+          label: "出生日期"
+        },
+        {
+          value: "首都",
+          label: "首都"
+        },
+        {
+          value: "身高",
+          label: "身高"
+        }
       ]
     };
   },
@@ -151,14 +217,16 @@ export default {
           .catch(error => {
             ElMessage.warning({
               message: error,
-              type: "warning"
+              type: "warning",
+              center: true
             });
           });
       } else {
         ElMessage.warning({
           message:
             "标注未完成，请完成标注后点击继续下一条标注或退出此次标注！！！",
-          type: "warning"
+          type: "warning",
+          center: true
         });
       }
     },
@@ -168,13 +236,15 @@ export default {
         let textLabel_dict = this.abstract_label.pop();
         this.text = textLabel_dict.text + this.text;
         ElMessage.success({
-          message: "取消" + textLabel_dict.text + "的标记",
-          type: "success"
+          message: '取消"' + textLabel_dict.text + '"的标记',
+          type: "success",
+          center: true
         });
       } else {
         ElMessage.warning({
           message: "没有被标注文本！！！",
-          type: "warning"
+          type: "warning",
+          center: true
         });
       }
     },
@@ -182,7 +252,8 @@ export default {
       if (this.text == null) {
         ElMessage.warning({
           message: "未开始文本标注，不需要保存！！！",
-          type: "warning"
+          type: "warning",
+          center: true
         });
       } else if (this.text.length == 0 && this.text != null) {
         this.$axios
@@ -205,13 +276,15 @@ export default {
           .catch(error => {
             ElMessage.warning({
               message: error,
-              type: "warning"
+              type: "warning",
+              center: true
             });
           });
       } else {
         ElMessage.warning({
           message: "标注未完成，请完成标注后进行保存！！！",
-          type: "warning"
+          type: "warning",
+          center: true
         });
       }
     },
@@ -226,9 +299,11 @@ export default {
             this.bagin = 0;
             this.text = null;
             this.flag = null;
+            this.spo_dict = null;
             this.text_pre = null;
             this.abstract = null;
             this.text_label = null;
+            this.spo_list = [];
             this.table_data = [];
             this.abstract_label = [];
             this.abstract = this.text = res.data.abstract;
@@ -241,38 +316,29 @@ export default {
           .catch(error => {
             ElMessage.error({
               message: error,
-              type: "error"
+              type: "error",
+              center: true
             });
           });
       } else {
         ElMessage.warning({
           message: "没有读取任何文本，没有文本被重置！！！",
-          type: "warning"
+          type: "warning",
+          center: true
         });
       }
     },
-    handleMouseSelect() {
+    handleMouseSelect(event) {
       if (window.getSelection().toString().length > 0) {
-        let loc = window
-          .getSelection()
-          .getRangeAt(0)
-          .getBoundingClientRect();
         this.bagin = window.getSelection().anchorOffset;
         this.end = window.getSelection().focusOffset;
         this.text_pre = this.flag = window.getSelection().toString();
         let el_radio_group = document.getElementsByClassName(
           "el-radio-group"
         )[0];
-        el_radio_group.style.position = "absolute";
-        el_radio_group.style.top = String(loc.bottom) + "px";
-        if (this.text_height == 0) {
-          el_radio_group.style.left = String(loc.left + loc.width) + "px";
-          this.text_height = loc.height;
-        } else if (this.text_height != 0 && loc.height == this.text_height) {
-          el_radio_group.style.left = String(loc.left + loc.width) + "px";
-        } else {
-          el_radio_group.style.left = String(loc.left + loc.left) + "px";
-        }
+        el_radio_group.style.position = "fixed";
+        el_radio_group.style.left = String(event.clientX) + "px";
+        el_radio_group.style.top = String(event.clientY) + "px";
       } else {
         this.flag = null;
         this.text_pre = null;
@@ -290,17 +356,35 @@ export default {
           text_label: this.text_label
         });
       } else {
-        ElMessage.warning({ message: "请按照顺序进行标注", type: "warning" });
+        ElMessage.warning({
+          message: "请按照顺序进行标注",
+          type: "warning",
+          center: true
+        });
       }
     },
-    tagClick() {
-      ElMessage.warning({ message: "tag点击事件", type: "warning" });
+    preLabel(label) {
+      this.spo_dict["predicate"] = label;
+      this.spo_list.push(this.spo_dict);
+      this.spo_dict = {};
+      this.predicate = null;
     },
-    verb_choose(event) {
+    verb_choose(text, event) {
       if (event.button == 0) {
-        ElMessage.info("你点了左键");
+        ElMessage.info("你点了左键" + event.clientX + " " + event.clientY);
+        if (Object.keys(this.spo_dict) == 0) {
+          this.spo_dict["subject_type"] = text;
+        } else {
+          this.predicate = this.spo_dict["object_type"] = text;
+          let select_trigger = document.getElementsByClassName(
+            "select-trigger"
+          )[0];
+          select_trigger.style.position = "fixed";
+          select_trigger.style.left = String(event.clientX) + "px";
+          select_trigger.style.top = String(event.clientY) + "px";
+        }
       } else if (event.button == 2) {
-        ElMessage.info("你点了右键");
+        ElMessage.info("你点了右键" + text);
       }
     }
   },
