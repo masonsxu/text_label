4362de1e8462f27f9d5581b4fa721deb66234374
masonsxu <masonsxu@foxmail.com>
2021-04-14 14:50:52 +0800

    更新格式化规则

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always'
+};
+
